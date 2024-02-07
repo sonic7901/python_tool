dd0f6336517ea1d29a1df11d44338245a5aecd91
Nick Wu <sonic7901@hotmail.com>
2024-02-07 13:38:01 +0800

    update gpt

--- apps/app_gpt/main.py
+++ apps/app_gpt/main.py
@@ -1,8 +1,41 @@
 from WebChatGPT import ChatGPT
-bot = ChatGPT(
-    "<path-to-openai-cookies.json>"
-)
-response = bot.chat('<Your prompt>')
+import speech_recognition as sr
+import pyttsx3
 
-print(response)
-#Ouput : What can I do for you today?
+# 初始化語音識別器和文字轉語音引擎
+recognizer = sr.Recognizer()
+tts_engine = pyttsx3.init()
+
+
+def read_chat(input_str):
+    bot = ChatGPT(
+        "chat.openai.com.cookies.json"
+    )
+    response = bot.chat(input_str)
+    return str(response)
+
+
+def set_speak():
+    print(sr.Microphone.list_working_microphones())
+    while True:
+        with sr.Microphone(device_index=1) as source:
+            # 調整麥克風收音閾值
+            recognizer.adjust_for_ambient_noise(source)
+            print("GPT: 聆聽中...")
+            audio = recognizer.listen(source)
+            try:
+                text_start = recognizer.recognize_google(audio, language='zh-TW')
+                print("User：", text_start)
+                if "朋友" in text_start:
+                    print("User：", text_start)
+                    temp_response = read_chat(text_start)
+                    print("GPT:" + temp_response)
+                    tts_engine.say(temp_response)
+                    tts_engine.runAndWait()
+            except Exception as e:
+                print("GPT: 無法識別")
+                tts_engine.say("抱歉，我沒聽懂，請再試一次")
+                print(e)
+
+
+set_speak()
